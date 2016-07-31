374ad42f45de49a038e66062580db8b18dc9e935
zr <zr@163.com>
2016-07-31 20:32:06 +0800

    修改viewCtr类

--- weixin/weixin/ViewController.m
+++ weixin/weixin/ViewController.m
@@ -16,12 +16,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the
+    NSLog(@"开始开发");
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
+
 
 @end
