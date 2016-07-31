eb55a2a3b5b9f0885efb38a2ed479c96d0946a0c
zr <zr@163.com>
2016-07-31 20:28:56 +0800

    初始化项目

---
+++ weixin/weixin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weixin
+//
+//  Created by etcxm on 16/7/31.
+//  Copyright © 2016年 中软国际. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ weixin/weixin/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  weixin
+//
+//  Created by etcxm on 16/7/31.
+//  Copyright © 2016年 中软国际. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
