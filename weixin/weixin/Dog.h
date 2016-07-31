22046ea0a5e26db0c33d412fecba0522f26175a3
zr <zr@163.com>
2016-07-31 20:34:00 +0800

    添加Dog类

---
+++ weixin/weixin/Dog.h
@@ -0,0 +1,13 @@
+//
+//  Dog.h
+//  weixin
+//
+//  Created by etcxm on 16/7/31.
+//  Copyright © 2016年 中软国际. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dog : NSObject
+
+@end
---
+++ weixin/weixin/Dog.m
@@ -0,0 +1,13 @@
+//
+//  Dog.m
+//  weixin
+//
+//  Created by etcxm on 16/7/31.
+//  Copyright © 2016年 中软国际. All rights reserved.
+//
+
+#import "Dog.h"
+
+@implementation Dog
+
+@end
